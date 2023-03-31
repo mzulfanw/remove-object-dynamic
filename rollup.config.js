73b7c4810999f6d3a9549d21d19cadee65e233d1
Muhammad Zulfan Wahyudin <mzulfan303@gmail.com>
2023-04-01 00:21:01 +0700

    fix: fixed again while build package

--- rollup.config.js
+++ rollup.config.js
@@ -3,13 +3,15 @@ import resolve from '@rollup/plugin-node-resolve'
 import commonjs from "@rollup/plugin-commonjs";
 
 export default {
-  input: './src/main.ts',
+  input: 'src/main.ts',
   output: {
-    file: './dist/main.js',
+    file: 'dist/main.js',
     format: 'cjs'
   },
   plugins: [
-    typescript(),
+    typescript({
+      tsconfig: './tsconfig.json'
+    }),
     resolve(),
     commonjs()
   ]
