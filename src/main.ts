d347cd01ff8653f98a927f59c27928368c2801e9
Muhammad Zulfan Wahyudin <mzulfan303@gmail.com>
2023-05-18 20:39:55 +0700

    feat: remove key if the deep object is empty

--- src/main.ts
+++ src/main.ts
@@ -24,13 +24,16 @@ export const removeObjectDynamic = (payload: Record<string, any>): Record<string
  * Remove object if has deep nesting value
  */
 export const removeObjectDeepDynamic = (payload: Record<string, any>): Record<string, any> => {
-  const filteredPayload: Record<string, any> = {}
+  const filteredPayload: Record<string, any> = {};
   for (const key in payload) {
     if (typeof payload[key] === 'object' && payload[key] !== null) {
-      filteredPayload[key] = removeObjectDeepDynamic(payload[key]);
-    } else if (payload[key] !== '' && payload[key] !== null && payload[key].length !== 0 && payload[key] !== undefined) {
-      filteredPayload[key] = payload[key]
+      const nestedObject = removeObjectDeepDynamic(payload[key]);
+      if (Object.keys(nestedObject).length !== 0) {
+        filteredPayload[key] = nestedObject;
+      }
+    } else if (payload[key] !== '' && payload[key] !== null) {
+      filteredPayload[key] = payload[key];
     }
   }
-  return filteredPayload
-}
+  return filteredPayload;
+};
